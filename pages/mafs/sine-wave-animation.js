9e6a4b2bfa8dd3dd9f12b04e1e26cb705a665efa
gsspdev <108854023+gsspdev@users.noreply.github.com>
2023-01-04 00:50:55 -0600

    used both yarn and npm to add mafs packages. if there is an issue with the package management then this is the first thing to look at

---
+++ pages/mafs/sine-wave-animation.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function CodePage() {
+    return (
+        <>
+            <Head>
+                <title>Code Page</title>
+            </Head>
+            <h1>maffy animations coming soon...</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+        </>
+    );
+    }
